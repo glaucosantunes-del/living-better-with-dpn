4e9f22d7ad9150168370fc1b899ebde759e85106
glaucosantunes-del <glaucosantunes@gmail.com>
2026-03-06 22:24:41 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
-export const metadata = {
-  title: "Living Better with DPN",
-  description: "Real-life experience with diabetes and diabetic peripheral neuropathy."
-}
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body style={{ fontFamily: "Arial", padding: "40px" }}>
+    <html>
+      <body>
         {children}
       </body>
     </html>
